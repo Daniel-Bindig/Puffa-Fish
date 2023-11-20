3a0b24d2aa0a321e9aa174d6ef135f3158b98032
Daniel Bindig <d.bindig@outlook.com>
2023-11-21 06:58:04 +1030

    Added TypeDefs

---
+++ Server/Schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const typeDefs = `
+  type Product {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    catagory: String!
+    imageUrl: String!
+    quantityAvailable: Int!
+  }
+
+  type Order {
+    _id: ID!
+    user: ID! # Assuming user ID for simplicity; replace with actual user type if available
+    products: [OrderProduct!]!
+    totalPrice: Float!
+    shippingAddress: String!
+  }
+  
+  type OrderProduct {
+    product: Product!
+    quantity: Int!
+    price: Float!
+
+  type Query {
+    products: [Product]
+    orders: [Order]
+  }
+
+`;
+
+module.exports = typeDefs;
