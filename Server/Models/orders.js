2955636c9e2896612bc5f282005a897ba0869890
Daniel Bindig <d.bindig@outlook.com>
2023-11-21 06:07:26 +1030

    Added models and server.js

---
+++ Server/Models/orders.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create the Order model based on the schema
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
